20a0f0017b32f2d63873b2b1d55f684786076524
Shubham Bhagat <CruxBox@users.noreply.github.com>
2019-10-29 15:27:01 +0530

    Create dijsktra.cpp

---
+++ dijsktra.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define forall(n) for(int i=0;i<n;i++)
+#define INF 10000
+
+class Graph{
+
+	int V;
+	vector<pair<int,int> > *adj;
+public:
+	Graph(int v){
+		V = v;
+		adj = new vector<pair<int,int> >[V];
+	}
+
+	void addEdge(int u, int v,int w){
+		adj[u].push_back(make_pair(v,w));
+		adj[v].push_back(make_pair(u,w));
+	}
+
+	void solve(int source){
+	
+		int dist[V];
+		bool vis[V];
+		forall(V){
+			vis[i] = false;
+			dist[i] = INF;
+		}
+		dist[source] = 0;
+
+		queue<int> q;
+
+		q.push(source);
+
+		while(q.size()){
+			int u = q.front();
+			q.pop();
+			vis[u] = 1;
+			for(int i = 0; i< adj[u].size();i++){
+				if(vis[adj[u][i].first] == 1) continue;
+				dist[adj[u][i].first] = min(dist[adj[u][i].first], dist[u] + adj[u][i].second);
+				q.push(adj[u][i].first);
+			}
+		}
+
+		int x;
+		cout<<"Queries: dest"<<endl;
+		while(x!=-1){
+			cin>>x;
+			cout<<dist[x]<<endl;
+		}
+	}
+};
+
+int main(){
+
+	int n,m;
+	cin>>n>>m;
+	int s;
+	cin>>s;
+	Graph g(n+1);
+
+	int x,y,z;
+	for(int i=0;i<m;i++){
+		cin>>x>>y>>z;
+		g.addEdge(x,y,z);
+	}
+
+	g.solve(s);
+
+}
